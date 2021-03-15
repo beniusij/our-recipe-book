aa8cfbef3ce02aa61d6f0f7059850863f3155bbf
Juozas <Juozas Beniusis>
2021-03-15 21:02:45 +0000

    Add portions component

---
+++ src/components/portions/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.scss'
+import icon from '../../images/dish.png'
+
+const Portions = ({ portionCount }) => {
+	const counter = portionCount === 1 ? portionCount + " portion" : portionCount + " portions" 
+
+	return (
+		<div class="portions">
+			<img src={icon} alt="Portions icon" className="portions__icon" />
+			<p className="portions__text">{counter}</p>
+		</div>
+	)
+}
+
+export default Portions
