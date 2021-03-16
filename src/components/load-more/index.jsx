79aacae23eee3b0876dd34ef68fee122955b9c87
Juozas <Juozas Beniusis>
2021-03-16 19:59:34 +0000

    Add load more button with styles, but no functionality

---
+++ src/components/load-more/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.scss'
+
+const LoadMore = (props) => {
+	return (
+		<div className="container--load-more">
+			<button className="load-more">
+				Load More >
+			</button>
+		</div>
+	)
+}
+
+export default LoadMore
