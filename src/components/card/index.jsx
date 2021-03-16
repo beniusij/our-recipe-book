88cc267fa5670f43b36039f4ade3f8ec8399996f
Juozas <Juozas Beniusis>
2021-03-16 19:30:51 +0000

    Update margin for cards

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 import Placeholder from '../../images/placeholder.jpg'
+import { Link } from 'gatsby'
 import './index.scss'
 
 const Card = (props) => {
 	const img = props.img ? props.img : Placeholder
 
 	return (
-		<div className='card'>
-			<div className='card__image-container' style={{backgroundImage: `url(${img})`,}}></div>
-			<div className='card__content'>
-				<h1 className='card__content--title'>{props.title}</h1>
-				<p className='card__content--timestamp'>{props.updatedAt}</p>
+		<Link to={props.link} className="card--link">
+			<div className='card'>
+				<div className='card__image-container' style={{backgroundImage: `url(${img})`,}}></div>
+				<div className='card__content'>
+					<h1 className='card__content--title'>{props.title}</h1>
+					<p className='card__content--timestamp'>{props.updatedAt}</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
 
