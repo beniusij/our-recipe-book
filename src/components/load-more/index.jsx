35e93a35ea54ecc6b130ebae65578b757b48c696
Juozas <Juozas Beniusis>
2021-03-17 17:42:13 +0000

    Update designs for load more button

--- src/components/load-more/index.jsx
+++ src/components/load-more/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 const LoadMore = (props) => {
 	return (
 		<div className="container--load-more">
-			<button className="load-more">
+			<button className="load-more" onClick={(e) => { console.log('Clicked load more') }}>
 				Load More >
 			</button>
 		</div>
